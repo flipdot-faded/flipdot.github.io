a444b66aba140c10d76bdd74b29835a95bd08f30
Jonas Platte <jplatte+git@posteo.de>
2017-08-27 20:06:05 +0200

    More work towards embedding the editor

--- js/edit.js
+++ js/edit.js
@@ -1,14 +1,23 @@
-window.wiki_runEditor = function() {
-    const editor = document.createElement("script");
-    editor.src = "/js/github_md_editor.js";
-    document.getElementsByTagName("head")[0].appendChild(editor);
+window.wiki_loadEditor = function() {
+    // From https://stackoverflow.com/a/39008859
+    return new Promise((resolve, reject) => {
+        const script = document.createElement("script");
+        script.async = true;
+        script.src = "/js/github_md_editor.js";
+        script.addEventListener("load", resolve);
+        script.addEventListener("error", () => reject("Error loading script."));
+        script.addEventListener("abort", () => reject("Script loading aborted."));
+        document.head.appendChild(script);
+    });
 };
 
 window.wiki_createPage = function(path) {
-    window.githubMdEditorConfig = {
-        mode: "CreatePage",
-        path: path,
-    };
+    // TODO: Loading animation
 
-    window.wiki_runEditor();
+    window.wiki_runEditor().then(() => {
+        window.startEditor(path, {
+            mode: "CreatePage",
+            path: path,
+        });
+    });
 };
